62809fcf06263299b13b8b141e7e2886ee6d5d1c
wilmer-kwong <35570658+wilmer-kwong@users.noreply.github.com>
2022-04-01 00:55:52 -0400

    git ingore next files

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
 // index.js
-import { useState } from 'react'
-function Header({ title }) {
-  return <h1>{title ? title : 'Default title'}</h1>
-}
+import { useState } from "react";
+import Link from "next/link";
 
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
 
-  const [likes, setLikes] = useState(0)
+  const [likes, setLikes] = useState(0);
 
   function handleClick() {
-    setLikes(likes + 1)
+    setLikes(likes + 1);
   }
 
   return (
     <div>
-      <Header title="Develop. Preview. Ship. 🚀" />
+      <Link href="/posts">
+        <a>Let's go</a>
+      </Link>
       <ul>
-        {names.map(name => (
+        {names.map((name) => (
           <li key={name}>{name}</li>
         ))}
       </ul>
 
       <button onClick={handleClick}>Like ({likes})</button>
     </div>
-  )
+  );
 }
---
+++ pages/posts/index.js
@@ -0,0 +1,5 @@
+import Link from 'next/link'
+
+export default function firstPost() {
+  return <Link href="/"><a>Back to home.</a></Link>
+}
